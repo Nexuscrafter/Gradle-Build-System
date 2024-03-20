3622a92bf4ffde5c631a211ee734e235607b832d
Shourya Bhattacharyya <shouryabhattacharyya@gmail.com>
2024-03-20 20:36:14 +0530

    Added Comments

--- src/main/java/org/example/TodoService.java
+++ src/main/java/org/example/TodoService.java
@@ -5,7 +5,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface TodoService {
-
+    //Create any number of api calls here with the below syntax
     @GET("/todos/{id}")
     Call<Todo> getTodoById(@Path("id") String id);
 }
